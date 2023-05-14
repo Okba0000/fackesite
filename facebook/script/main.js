ef673c1c2d140358ade63514a24ee6f87d0e7d77
Okba0000 <93545249+Okba0000@users.noreply.github.com>
2023-05-14 13:29:11 +0100

    Add files via upload

---
+++ facebook/script/main.js
@@ -0,0 +1,5 @@
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function(e){
+	e.preventDefault();
+	console.log('hi');
+})
---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+var name;
+function subm() {
+	name = document.getElementById('name').value;
+	alert('مرحبا '+name+' لأجل ضمان التواصل معك يرجى ربط حسابك الخاص بالفايسبوك ')
+	location.replace('file:///C:/Users/LeoTech/Desktop/task/facebook/facebook.html')
+}
